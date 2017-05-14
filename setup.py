f46dba0df0966cac4d7b0db155028c940919331f
Bigo <bigo@crisidev.org>
2017-05-14 21:29:03 +0100

    Bump version to 0.3 to fix setup.py errors

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import sys
-import codecs
 from distutils.core import setup
 from setuptools import find_packages, Command
+
+VERSION = 0.3
 
 
 class FormatCommand(Command):
@@ -23,36 +24,34 @@ cmdclass = {
     'format': FormatCommand
 }
 
-with codecs.open('README.md', 'r', 'utf-8') as fd:
-    setup(
-        name='vault-ca',
-        version='0.1',
-        description='Set of utils to create your own CA using hashicorp Vault',
-        long_description=fd.read(),
-        author='Matteo Bigoi',
-        author_email='bigo@crisidev.org',
-        url='https://github.com/crisidev/vault-ca',
-        license='GPLv3',
-        download_url='https://github.com/crisidev/vault-ca/archive/0.1.tar.gz',
-        keywords=['ssl', 'certificate-authority', 'vault'],
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Programming Language :: Unix Shell',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-        ],
-        packages=find_packages(exclude=['docs', 'tests']),
-        scripts=['bin/fetch-ssl-cert', 'bin/create-vault-ca'],
-        install_requires=['appdirs', 'pyparsing', 'pyopenssl', 'requests'],
-        cmdclass=cmdclass,
-        setup_requires=['pytest-runner'],
-        tests_require=['pytest', 'pytest-cov', 'requests-mock']
-    )
+setup(
+    name='vault-ca',
+    version='{}'.format(VERSION),
+    description='Set of utils to create your own CA using hashicorp Vault',
+    author='Matteo Bigoi',
+    author_email='bigo@crisidev.org',
+    url='https://github.com/crisidev/vault-ca',
+    license='GPLv3',
+    download_url='https://github.com/crisidev/vault-ca/archive/{}.tar.gz'.format(VERSION),
+    keywords=['ssl', 'certificate-authority', 'vault'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Unix Shell',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['docs', 'tests']),
+    scripts=['bin/fetch-ssl-cert', 'bin/create-vault-ca'],
+    install_requires=['appdirs', 'pyparsing', 'pyopenssl', 'requests'],
+    cmdclass=cmdclass,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-cov', 'requests-mock']
+)
